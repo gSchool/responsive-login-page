503bed9dd3a540518b7b5f52a2209b2160ef5305
Dennis Bailey <bailey.dennis@gmail.com>
2016-02-16 22:05:55 -0700

    Added a DOM injection welcome message

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).on('ready', function() {
+  console.log('Ready, PLAYER ONE?');
+  
+  $('body').append('<h3 class ="center">There are many responsive login pages out there, but this one\'s mine.</h3>');
+
+});
