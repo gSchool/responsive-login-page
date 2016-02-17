4d14266e4f28e64347d1a93161bd8973ca101d87
Dennis Bailey <bailey.dennis@gmail.com>
2016-02-17 08:43:02 -0700

    Added a six character minimum ot the password field

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).on('ready', function() {
   console.log('Ready, PLAYER ONE?');
   
-  $('body').append('<h3 class ="center">There are many responsive login pages out there, but this one\'s mine.</h3>');
+  
 
 });
 
 $(document).on('click', '#submit', function(){
     // Prevent event submission
     event.preventDefault();
-    
+    $('body').append('<h3 class ="center">There are many responsive login pages out there, but this one\'s mine.</h3>');
 });
