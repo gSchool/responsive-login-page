1e15a5b0cd86115b3e779cb778cbbb0826e74712
Daniel Klein <danhirschklein@gmail.com>
2016-02-17 01:11:41 -0700

    Added some responsive jquery

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('.submit').on('click', function(){
+  var mail = $('#mail').val();
+  var psw = $('#psw').val();
+  var newLabel = '<label for="correct">Is '+ mail +' correct?</label><br>';
+  var newInputs ='<input type="button" class="yes" value="Yes!"><input type="button" class="cancel" value="Cancel">'
+
+  $('fieldset').html(newLabel + newInputs)
+})
+
+$('.yes').on('click', function(){
+  $('fieldset').empty().append("Great! Have a fantastic day!");
+
+})
