f39fee857f2e85592516c8158ea27fc65ebb5e38
millsmcilroy <millsmcilroy@gmail.com>
2016-02-17 09:21:21 -0700

    made welcome msg create in memory

--- scripts.js
+++ scripts.js
@@ -5,7 +5,9 @@ $('button').click(function() {
   //If form is valid
   if ($form[0].checkValidity()) {
     event.preventDefault();
+    var $welcome = $('<h2>Welcome!</h2>');
     $('.largeView').hide();
     $('#welcomeScreen').show();
+    $('#welcomeScreen').append($welcome);
   }
 });
