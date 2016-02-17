4c4c3d4bdf1d50da61c9bff58b84cef0d5094f3b
Louisa Reese <louisa.e.reese@gmail.com>
2016-02-16 21:01:36 -0700

    added variables for input values

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 });
+
+$("form").on("submit", function(event){
+	event.preventDefault();
+
+	var email = $("#email").val();
+	var password = $("#password").val();
+	console.log(email, password)
+
+// empty inputs - not working
+	$("#email").text("");
+	$("#password").text("");
+
+});
