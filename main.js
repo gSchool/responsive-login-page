70477946976d0dc4fc6096b5133f77db6ec5f6dd
Kathy Dieser <katdiese@gmail.com>
2016-02-17 08:31:40 -0700

    added validation to form submission

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-  console.log("hello world!");
-});
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+
+var validateForm = function(event) {
+  event.preventDefault();
+  var pass = document.forms['myForm']['password'].value;
+  if(pass.split("").length <= 6) {
+    alert("Password must be longer than 6 characters");
+  } else {
+    alert("Thanks for logging in!");
+    email.value = "";
+    password.value = "";
+  }
+}
