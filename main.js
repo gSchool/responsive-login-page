5c0dea0aff36452d64fb05a2140cd1e045a17d7a
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-02-21 15:11:49 -0700

    completed

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  console.log('sanity check');
+
+  $("#login").on("click", function(){
+    var email = $($("input")[0]).val();
+    var password = $($("input")[1]).val();
+    if(email && password && password.length > 6 && email.indexOf('@' )!== -1) {
+      alert("welcome");
+    }
+  })
+
 })
